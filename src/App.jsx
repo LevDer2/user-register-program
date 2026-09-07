79b215c5db41ecb3a9f2108d3d8063a1f40577f2
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2026-09-07 19:21:13 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Contacts from "./pages/Contacts";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="contacts" element={<Contacts />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,11 @@
+import AuthNav from "../AuthNav/AuthNav";
+import Navigation from "../Navigation/Navigation";
+
+export default function AppBar() {
+  return (
+    <>
+      <Navigation />
+      <AuthNav />
+    </>
+  );
+}
